ca75fbda735ec7395833e27ddffc00010df4e885
Nikolai Krot <talpus@gmail.com>
2013-03-27 00:52:29 +0300

    Morphotype can now be configured to return either _RARE_ or classes of _RARE_ (ALLCAPS,NUMERIC,etc) via a single query interface

--- taggers/lib/morphotype.rb
+++ taggers/lib/morphotype.rb
@@ -1,10 +1,28 @@
 
 class Morphotype
 
-#Numeric The word is rare and contains at least one numeric characters.
-#All Capitals The word is rare and consists entirely of capitalized letters.
-#Last Capital The word is rare, not all capitals, and ends with a capital letter.
-#Rare The word is rare and does not fit in the other classes.
+  # Numeric The word is rare and contains at least one numeric characters.
+  # All Capitals The word is rare and consists entirely of capitalized letters.
+  # Last Capital The word is rare, not all capitals, and ends with a capital letter.
+  # Rare The word is rare and does not fit in the other classes.
+
+  def initialize(klass=:single)
+    set_classifier(klass)
+  end
+
+  # valid values:
+  #  :single, :multi
+  def use_type_of_rare=(klass)
+    set_classifier(klass)
+  end
+
+  # universal interface
+  def type_of_rare(word)
+    @classifier.call(word)
+  end
+
+  ####################################################################
+  # low levels
 
   def type_of(word)
     case word
@@ -19,5 +37,20 @@ class Morphotype
 
   def rare
     "_RARE_"
+  end
+
+  ####################################################################
+  private
+  ####################################################################
+
+  def set_classifier(klass)
+    case klass
+    when :single
+      @classifier = lambda {|word| rare}
+    when :multi
+      @classifier = lambda {|word| type_of(word) || rare }
+    else
+      raise "Unknown type of rare requested: #{klass}"
+    end
   end
 end
